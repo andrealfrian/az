7d141a1184a9ad917c74f023a71e5b09c07b911f
andrealfrian <118164813+andrealfrian@users.noreply.github.com>
2022-11-19 09:19:38 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+  function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    
+  }
+
